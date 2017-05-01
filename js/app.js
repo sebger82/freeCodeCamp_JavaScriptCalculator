7a60c647aa63fb021764559dcf6d9f0d785172bb
Sebastian Urbanski <sebger82@gmail.com>
2017-05-01 11:49:44 +0200

    poprwilem blad ze zwielokrotnianiem zer

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 // add value from id
 function takeValueFromId(id) {
-    if (document.calculator.screen.value == 0) {
+    if (document.calculator.screen.value === "0") {
     document.calculator.screen.value = id;
         } else {
            document.calculator.screen.value += id; 
         }
 }
-
+function specialtakeValueFromId(id) {
+    document.calculator.screen.value += id;
+}
 // Calculate base on current value
 function calculate() {
     try {
